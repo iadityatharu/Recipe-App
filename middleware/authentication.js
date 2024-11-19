2ee6465c55b1821481e270308e55871aea575d39
LearnerAdityaa <adityatharudev@gmail.com>
2024-11-19 21:37:29 +0545

    feat:added authentication middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import { expressError } from "../utils/expressError.js";
+export const authentication = (req, res, next) => {
+  const token = req.cookies.accessToken;
+  if (!token) {
+    throw new expressError(401, "Authentication token is expired");
+  }
+  jwt.verify(token, process.env.ACCESSTOKEN, (err, user) => {
+    if (err) {
+      console.log(err);
+      throw new expressError(403, "Token is expired, Please sign in again");
+    }
+    req.user = user;
+    next();
+  });
+};
