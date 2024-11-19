6d9a108adc662ffbbb260d5db6dc1abdb735b417
LearnerAdityaa <adityatharudev@gmail.com>
2024-11-19 21:28:30 +0545

    feat:added checkEnv file for checking all environment

---
+++ function/checkEnv.js
@@ -0,0 +1,18 @@
+const requiredEnvVar = [
+  "PORT",
+  "ACCESSTOKEN",
+  "ACCESSEXPIRE",
+  "REFRESHTOKEN",
+  "REFRESHEXPIRE",
+  "MONGO_URL",
+  "NODEMAILER_PASS",
+  "NODEMAILER_USER",
+];
+export const missingVar = () => {
+  const missedVar = requiredEnvVar.filter((envVar) => !process.env[envVar]);
+  if (missedVar.length > 0) {
+    console.log("variable is missing in env file");
+    console.log(missedVar);
+    process.exit(1);
+  }
+};
