70e45d0423187e0c37a001edc986b9e8db9e19cf
LearnerAdityaa <adityatharudev@gmail.com>
2024-11-19 21:44:40 +0545

    feat:added all necessary service

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,2 +1,14 @@
-import User from "../model/user.model";
-
+import { signup as signupService } from "../services/user.service.js";
+import { signin as signinService } from "../services/user.service.js";
+import { getUserInfo as getUserInfoService } from "../services/user.service.js";
+import { updateAddres as updateAddresService } from "../services/user.service.js";
+import { generateToken as generateTokenService } from "../services/user.service.js";
+import { sendOtp as sendOtpService } from "../services/user.service.js";
+import { forgotPassword as forgotPasswordService } from "../services/user.service.js";
+import { changePassword as changePasswordService } from "../services/user.service.js";
+import { search as searchService } from "../services/user.service.js";
+import { expressError } from "../utils/expressError.js";
+import jwt from "jsonwebtoken";
+import { accessExpiry, refreshExpiry } from "../constant.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import { generateOtp } from "../function/generateOtp.js";
