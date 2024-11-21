5f2776a5904b87b34da2bea4817c35dc5aa597c7
LearnerAdityaa <adityatharudev@gmail.com>
2024-11-21 08:37:10 +0545

    feat:added signin function

--- services/user.service.js
+++ services/user.service.js
@@ -21,3 +21,23 @@ export const signup = async (validUser) => {
   await newUser.save();
   return { status: 201 };
 };
+export const signin = async (userData) => {
+  const { email, password } = userData;
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    return { status: 404 };
+  }
+  const isValidPassword = await bcrypt.compare(password, existingUser.password);
+  if (!isValidPassword) {
+    return { status: 403 };
+  }
+  const authClaims = {
+    id: existingUser._id,
+    role: existingUser.role,
+  };
+  const newAccessToken = await accessToken(authClaims);
+  const newRefreshToken = await refreshToken(authClaims.id);
+  existingUser.refreshToken = newRefreshToken;
+  await existingUser.save();
+  return { newAccessToken, newRefreshToken };
+};
