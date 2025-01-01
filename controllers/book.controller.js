e8c8c3bd9066e30d334fa40c098e46a0485ebf01
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-01 21:32:01 +0545

    fix: fix bugs

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,58 +1,49 @@
 import {
-  addBook as addBookService,
-  updateBook as updateBookService,
+  addRecipe as addRecipeService,
+  updateRecipe as updateRecipeService,
   updatePrice as updatePriceService,
-  deleteBook as deleteBookService,
-  getAllBook as getAllBookService,
+  deleteRecipe as deleteRecipeService,
+  getAllRecipe as getAllRecipeService,
   search as searchService,
-  getRecentBook as getRecentBookService,
-  updateDiscount as updateDiscountService,
-  getSpecificBook as getSpecificBookService,
-  exportToExcel as exportToExcelService,
+  getRecentRecipe as getRecentRecipeService,
+  getSpecificRecipe as getSpecificRecipeService,
 } from "../service/book.service.js";
-export const addBook = async (req, res) => {
+export const addRecipe = async (req, res) => {
   const validBook = req.validBook;
-  const response = await addBookService(validBook);
+  const response = await addRecipeService(validBook);
   return res.status(200).json({ message: response });
 };
-export const updateBook = async (req, res) => {
-  const { bookid } = req.headers;
-  const response = await updateBookService(bookid, req.body);
+export const updateRecipe = async (req, res) => {
+  const { recipeid } = req.headers;
+  const response = await updateRecipeService(recipeid, req.body);
   return res.status(200).json({ message: response });
 };
 export const updatePrice = async (req, res) => {
-  const { bookid } = req.headers;
+  const { recipeid } = req.headers;
   const { price } = req.body;
-  const response = await updatePriceService(bookid, price);
+  const response = await updatePriceService(recipeid, price);
   return res.status(201).json({ message: response });
 };
-export const deleteBook = async (req, res) => {
-  const { bookid } = req.headers;
-  const response = await deleteBookService(bookid);
+export const deleteRecipe = async (req, res) => {
+  const { recipeid } = req.headers;
+  const response = await deleteRecipeService(recipeid);
   return res.status(200).json({ message: response });
 };
-export const updateDiscount = async (req, res) => {
-  const { bookid } = req.headers;
-  const { discount } = req.body;
-  const response = await updateDiscountService(bookid, discount);
-  return res.status(201).json({ message: response });
-};
-export const getAllBook = async (req, res) => {
-  const books = await getAllBookService();
-  return res.status(200).json({ status: true, data: books });
+export const getAllRecipe = async (req, res) => {
+  const recipes = await getAllRecipeService();
+  return res.status(200).json({ status: true, data: recipes });
 };
 export const search = async (req, res) => {
-  const { title, price, author } = req.body;
-  const response = await searchService(title, price, author);
+  const { title, price, chef } = req.body;
+  const response = await searchService(title, price, chef);
   res.status(200).json({ response });
 };
-export const getRecentBook = async (req, res) => {
-  const books = await getRecentBookService();
-  return res.status(200).json({ status: "success", data: books });
+export const getRecentRecipe = async (req, res) => {
+  const recipes = await getRecentRecipeService();
+  return res.status(200).json({ status: "success", data: recipes });
 };
-export const getSpecificBook = async (req, res) => {
+export const getSpecificRecipe = async (req, res) => {
   const { id } = req.params;
-  const book = await getSpecificBookService(id);
-  return res.status(200).json({ status: "success", data: book });
+  const recipe = await getSpecificRecipeService(id);
+  return res.status(200).json({ status: "success", data: recipe });
 };
-
