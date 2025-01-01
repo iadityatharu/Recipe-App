30355bbf6fb7dfd106c36d0534ed56e4fb84e702
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-02 01:12:58 +0545

    fix: fix bugs

---
+++ routes/order.route.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { authentication } from "../middleware/userAuth.js";
+import { wrapAsync } from "../utils/wrapAsync.js";
+import {
+  getAllOrder,
+  orderHistory,
+  placeOrder,
+  exportToExcel,
+  search,
+  updateOrderAdmin,
+} from "../controllers/order.controller.js";
+import { isAdmin } from "../middleware/isAdmin.js";
+const router = express.Router();
+// place order
+router.post("/place-order", authentication, wrapAsync(placeOrder));
+// get order-history of particular user
+router.get("/get-order-history", authentication, wrapAsync(orderHistory));
+
+// get all orders ---admin
+router.get("/get-all-order", authentication, isAdmin, wrapAsync(getAllOrder));
+// update order admin only
+router.patch(
+  "/update-status/:id",
+  authentication,
+  isAdmin,
+  wrapAsync(updateOrderAdmin)
+);
+router.get("/search", authentication, isAdmin, wrapAsync(search));
+export default router;
