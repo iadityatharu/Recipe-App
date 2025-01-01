bd30ca623e4927c2621e0735e464f4f632567cd3
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-01 20:34:05 +0545

    fix: fix bugs

---
+++ routes/user.route.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { wrapAsync } from "../util/wrapAsync.js";
+import { validSignin, validUser } from "../middleware/validate.js";
+import { authentication } from "../middleware/userAuth.js";
+import { changePasswordCheck } from "../middleware/changePasswordCheck.js";
+import {
+  signup,
+  signin,
+  getUserInfo,
+  updateAddress,
+  search,
+  exportToExcel,
+  generateToken,
+  logout,
+  forgotPassword,
+  changePassword,
+  sendOtp,
+} from "../controller/user.controller.js";
+import { checkRefreshToken } from "../middleware/checkRefreshToken.js";
+import { isAdmin } from "../middleware/isAdmin.js";
+const router = express.Router();
+router.post("/signup", validUser, wrapAsync(signup));
+router.post("/signin", validSignin, wrapAsync(signin));
+router.get("/get-user-information", authentication, wrapAsync(getUserInfo));
+router.patch("/update-address", authentication, wrapAsync(updateAddress));
+router.post("/refresh-token", checkRefreshToken, wrapAsync(generateToken));
+router.delete("/logout", authentication, wrapAsync(logout));
+router.patch(
+  "/change-password",
+  authentication,
+  changePasswordCheck,
+  wrapAsync(changePassword)
+);
+router.post("/send-otp", wrapAsync(sendOtp));
+router.post("/forgot-password", wrapAsync(forgotPassword));
+router.get("/search", authentication, isAdmin, wrapAsync(search));
+router.get("/download-excel", authentication, wrapAsync(exportToExcel));
+export default router;
