ddb858ebb63152ca60624e5aea7735e7910b3504
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-02 01:16:49 +0545

    fix: fix bugs on wrapper route

--- routes/wraperRoute.route.js
+++ routes/wraperRoute.route.js
@@ -1,9 +1,12 @@
 import express from "express";
 import userRoute from "./user.route.js";
 import favouriteRoute from "./favourite.route.js";
+import orderRoute from "./order.route.js";
+import recipeRoute from "./recipe.route.js";
 
 const router = express.Router();
 router.use("/user", userRoute);
 router.use("/favourite", favouriteRoute);
-
+router.use("/order", orderRoute);
+router.use("/recipe", recipeRoute);
 export default router;
