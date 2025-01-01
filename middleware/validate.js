55d52f2b8cefdc75987665c34bcd0f1d11d1ce54
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-01 20:31:07 +0545

    fix : fix bugs

--- middleware/validate.js
+++ middleware/validate.js
@@ -18,38 +18,3 @@ export const validUser = async (req, res, next) => {
     next(new expressError(400, "Invalid user data"));
   }
 };
-export const validBook = (req, res, next) => {
-  try {
-    const { err, value: validUrl } = validateUrl.validate(req.file);
-    if (err) {
-      throw new expressError(400, err.message);
-    }
-    const { error, value: validBook } = validateBook.validate(req.body);
-    if (error) {
-      throw new expressError(400, error.message);
-    }
-    req.validBook = validBook;
-    req.validUrl = validUrl;
-    next();
-  } catch (error) {
-    throw new expressError(400, error.message);
-  }
-};
-export const validSignin = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const { error, value: validSignin } = validateSignin.validate({
-      email,
-      password,
-    });
-    if (error) {
-      throw new expressError(400, error.message);
-    }
-    req.validSignin = validSignin;
-    next();
-  } catch (error) {
-    next(error);
-  }
-};
-
-
