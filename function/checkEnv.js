0d766c13fee3c8ffd7978df9f162118d38432a4b
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-02 01:45:50 +0545

    fix: fix bugs

--- function/checkEnv.js
+++ function/checkEnv.js
@@ -8,6 +8,10 @@ const requiredEnvVar = [
   "NODEMAILER_PASS",
   "NODEMAILER_USER",
   "STRIPE_SECRET_KEY",
+  "CLOUDINARY_CLOUD_NAME",
+  "CLOUDINARY_API_KEY",
+  "CLOUDINARY_API_SECRET",
+  "STRIPE_SECRET_KEY",
 ];
 export const missingVar = () => {
   const missedVar = requiredEnvVar.filter((envVar) => !process.env[envVar]);
