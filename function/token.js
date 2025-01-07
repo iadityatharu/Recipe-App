1dceffc26351798b5ec19c9dee3f8134562afc6c
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-07 20:15:05 +0545

    fix:fix bugs

--- function/token.js
+++ function/token.js
@@ -4,9 +4,7 @@ export const accessToken = async (authClaims) => {
   if (!process.env.ACCESSTOKEN || !process.env.ACCESSEXPIRE) {
     throw new expressError(400, "Token configuration is missing");
   }
-  const accessToken = jwt.sign({ authClaims }, process.env.ACCESSTOKEN, {
-    expiresIn: process.env.ACCESSEXPIRE,
-  });
+  
   return accessToken;
 };
 export const refreshToken = async (userId) => {
