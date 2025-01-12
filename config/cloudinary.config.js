0f1bac2ad2c3f3b01741592f6dad57f71818706f
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-12 17:05:20 +0545

    fix:remove useless comments

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -8,30 +8,22 @@ cloudinary.v2.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-// Utility function to upload an image to Cloudinary
 const imageUploadUtil = async (file) => {
   try {
     if (!file || !file.buffer) {
       throw new expressError(400, "No valid file provided for upload.");
     }
-
-    // Check if the file is an image (optional validation step)
     if (!file.mimetype.startsWith("image/")) {
       throw new expressError(400, "Only image files are allowed.");
     }
-
-    // Create a readable stream from the file buffer
     const stream = streamifier.createReadStream(file.buffer);
-
-    // Upload the stream to Cloudinary
     const uploadResult = await new Promise((resolve, reject) => {
       cloudinary.v2.uploader
         .upload_stream(
           {
             resource_type: "image",
             folder: "recipe_images",
-            public_id: `${Date.now()}-${file.originalname}`, // Add timestamp for uniqueness
+            public_id: `${Date.now()}-${file.originalname}`, 
             overwrite: true,
           },
           (error, result) => {
