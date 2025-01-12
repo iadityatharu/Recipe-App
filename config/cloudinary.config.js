467b61bcaeba88a178c9e5874dbe35f63e0a997d
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-12 12:33:34 +0545

    fix:fix bugs and add limitter as well as extension check

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -1,29 +1,53 @@
 import cloudinary from "cloudinary";
 import multer from "multer";
 import { expressError } from "../utils/expressError.js";
-
+// Cloudinary configuration
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+// Multer storage configuration
 const storage = multer.memoryStorage();
+const upload = multer({
+  storage,
+  limits: { fileSize: 10 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    const allowedTypes = ["image/jpeg", "image/png", "image/webp"];
+    if (allowedTypes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(
+        new expressError(
+          400,
+          "Unsupported file type. Only JPG, PNG, and WEBP are allowed."
+        )
+      );
+    }
+  },
+});
+
+// Image upload utility
 const imageUploadUtil = async (files) => {
   try {
     if (files.length > 8) {
       throw new expressError(400, "You can only upload a maximum of 8 images.");
     }
     const uploadPromises = files.map((file) =>
-      cloudinary.v2.uploader.upload(file.buffer, {
-        resource_type: "auto",
-      })
+      cloudinary.v2.uploader.upload_stream(
+        { resource_type: "auto" },
+        file.buffer
+      )
     );
     const results = await Promise.all(uploadPromises);
     const urls = results.map((result) => result.secure_url);
     return urls;
   } catch (error) {
-    throw new expressError(400, "Unable to upload image.");
+    throw new expressError(
+      400,
+      `Unable to upload image. Error: ${error.message}`
+    );
   }
 };
-const upload = multer({ storage });
+
 export { upload, imageUploadUtil };
