d2605f3242894c831d13896d2f85c4a7fe6a4e56
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-12 17:01:53 +0545

    remove:useless comment

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -4,8 +4,6 @@ import { imageUploadUtil } from "../config/cloudinary.config.js"; // Assumes Clo
 
 // Configure Multer to use memory storage
 const storage = multer.memoryStorage();
-
-// Set up Multer middleware for single image upload
 const upload = multer({
   storage,
   limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5 MB
@@ -22,24 +20,14 @@ const upload = multer({
     cb(null, true);
   },
 }).single("image");
-
-// Middleware for uploading the image to Cloudinary
 const uploadImageHandler = async (req, res, next) => {
   try {
     const uploadedFile = req.file;
-
-    // Check if a file was uploaded
     if (!uploadedFile) {
       throw new expressError(400, "No image file provided for upload.");
     }
-
-    // Upload the image to Cloudinary
     const imageUrl = await imageUploadUtil(uploadedFile);
-
-    // Attach the uploaded image URL to the request object
     req.imageUrl = imageUrl;
-
-    // Proceed to the next middleware or controller
     next();
   } catch (error) {
     next(error);
