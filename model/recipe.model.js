1045c749c9839d05135f8abc8bf48ecd9136ac4e
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-12 10:48:48 +0545

    feat:added recipe model after updated it

--- model/recipe.model.js
+++ model/recipe.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+
 const recipeSchema = new mongoose.Schema(
   {
     title: {
@@ -14,20 +15,32 @@ const recipeSchema = new mongoose.Schema(
       required: true,
     },
     ingredients: {
-      type: [String],
+      type: [
+        {
+          name: { type: String, required: true }, // Ingredient name
+          quantity: { type: Number, required: true }, // Quantity of the ingredient
+          unit: { type: String, required: true }, // Unit of measurement (e.g., grams, cups)
+        },
+      ],
       required: true,
     },
     process: {
-      type: String,
+      type: [
+        {
+          step_number: { type: Number, required: true }, // Step number for ordering
+          description: { type: String, required: true }, // Detailed description of the step
+        },
+      ],
       required: true,
     },
     images: {
-      type: [String],
+      type: [String], // URLs or paths to recipe images
       required: false,
     },
   },
   { timestamps: true }
 );
+
 const Recipe = mongoose.model("Recipe", recipeSchema);
 
 export default Recipe;
