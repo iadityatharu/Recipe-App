1bcee90d2c1dc3d484fafec8b0b1e2f3349a9a32
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-01-12 12:01:48 +0545

    feat:added recipe adding controller

--- controllers/recipe.controller.js
+++ controllers/recipe.controller.js
@@ -7,10 +7,10 @@ import {
   getRecentRecipe as getRecentRecipeService,
   getSpecificRecipe as getSpecificRecipeService,
 } from "../services/recipe.service.js";
-import { expressError } from "../utils/expressError.js";
 import { imageUploadUtil } from "../config/cloudinary.config.js";
 export const addRecipe = async (req, res) => {
   const { title, description, price, ingredients, process } = req.body;
+  // Handle image uploads
   const uploadedImages = req.files;
   let imageUrls = [];
   if (uploadedImages && uploadedImages.length > 0) {
@@ -21,32 +21,16 @@ export const addRecipe = async (req, res) => {
       "https://www.gettyimages.com/detail/photo/men-eating-vegan-creamy-roasted-pumpkin-soup-royalty-free-image/1197494143",
     ];
   }
-  // Validate ingredients and process input
-  const formattedIngredients = JSON.parse(ingredients).map((ingredient) => {
-    if (!ingredient.name || !ingredient.quantity || !ingredient.unit) {
-      throw new expressError(400, "Invalid ingredient format.");
-    }
-    return ingredient;
-  });
-  const formattedProcess = JSON.parse(process).map((step, index) => {
-    if (!step.description) {
-      throw new expressError(400, "Each process step must have a description.");
-    }
-    return {
-      step_number: index + 1, // Assign step numbers if not provided
-      description: step.description,
-    };
-  });
   const validRecipe = {
     title,
     description,
     price,
-    ingredients: formattedIngredients,
-    process: formattedProcess,
+    ingredients,
+    process,
     images: imageUrls,
   };
   const response = await addRecipeService(validRecipe);
-  return res.status(200).json({ message: "Recipe added successfully" });
+  return res.status(200).json({ message: response });
 };
 
 export const updateRecipe = async (req, res) => {
