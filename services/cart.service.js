bd170b72fc5fb7a50929afa1e118921578ea7564
Aditya Tharu <159002878+iadityatharu@users.noreply.github.com>
2025-02-12 23:35:31 +0545

    feat:added backend functionality to add in cart

--- services/cart.service.js
+++ services/cart.service.js
@@ -1,20 +1,21 @@
 import User from "../model/user.model.js";
 export const addToCart = async (recipeid, userId) => {
   const userData = await User.findById(userId);
-  const isBookInCart = userData.carts.includes(recipeid);
-  if (isBookInCart) {
+  const isRecipeInCart = userData.carts.includes(recipeid);
+  if (isRecipeInCart) {
     return "Recipe already in cart";
   }
-  await User.findByIdAndUpdate(id, {
-    $push: { cart: bookid },
+  await User.findByIdAndUpdate(userId, {
+    $push: { carts: recipeid },
   });
   return "Recipe added to cart";
 };
 export const removeCart = async (recipeid, userId) => {
-  await User.findByIdAndUpdate(userId, { $pull: { cart: recipeid } });
+  await User.findByIdAndUpdate(userId, { $pull: { carts: recipeid } });
   return "Recipe removed from cart";
 };
 export const getAllCart = async (userId) => {
   const userData = await User.findById(userId).populate("carts");
-  return userData.carts.reverse();
+  console.log(userData.carts);
+  return userData.carts;
 };
